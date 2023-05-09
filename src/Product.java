343043be67f295f62f6a85cfd3074499d6e6d023
Arkady <djony23rus@mail.ru>
2023-05-09 17:40:13 +0200

    Сдесь я сделал, базу ввиде продуктов, без наследования и создание конструктора.

---
+++ src/Product.java
@@ -0,0 +1,12 @@
+public class Product {
+    protected String brand;
+    protected String name;
+    protected double price;
+    protected double weight;
+    public Product(){
+
+    }
+    public Product(String brand, String name, double price, double weight){
+
+    }
+}
